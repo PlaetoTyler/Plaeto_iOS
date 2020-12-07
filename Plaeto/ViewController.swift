45ac1274204fbfad4d58b4bdd88eedceda48b4df
Tyler <tyler@plaetoapp.com>
2020-12-07 16:22:33 -0500

    Initial Test

--- Plaeto/ViewController.swift
+++ Plaeto/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var error: UILabel!
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        if name.text == ""{
+            error.text = "please enter a name"
+            error.isHidden = false
+            return
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
